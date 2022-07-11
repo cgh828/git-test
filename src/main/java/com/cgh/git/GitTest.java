76bd5e52c554b4f4364c2fb1e7a9849bb9d97f21
Cgh <Cgh@uii-ai.com>
2022-07-11 22:20:43 +0800

    first commit

---
+++ src/main/java/com/cgh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cgh.git;
+
+/**
+ * @author cgh
+ * @date 2022/7/11 22:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
